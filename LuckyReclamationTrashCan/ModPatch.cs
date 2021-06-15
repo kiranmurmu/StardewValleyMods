851c7de93c00fb144e7b78de3a8d7020b92064c5
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2021-06-15 21:44:42 +0530

    Added mod patch method

---
+++ LuckyReclamationTrashCan/ModPatch.cs
@@ -0,0 +1,15 @@
+﻿using StardewValley;
+
+namespace KiranMurmu.StardewValleyMods.LuckyReclamationTrashCan
+{
+  /// <summary>The mod patch for Harmony</summary>
+  internal class ModPatch
+  {
+    /// <summary>A method called via Harmony after <see cref="StardewValley.Utility.getTrashReclamationPrice(Item, Farmer)"/>.</summary>
+    /// <param name="__result">The return value.</param>
+    public static void After_getTrashReclamationPrice(ref int __result)
+    {
+      __result = (int)(__result / (double)(0.321 - (double)Game1.player.LuckLevel * 0.05 - Game1.player.DailyLuck));
+    }
+  }
+}
