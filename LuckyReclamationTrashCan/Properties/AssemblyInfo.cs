cb6df057ff523e574e5c39acba13fcc8c6a3635f
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2021-06-16 22:59:04 +0530

    Updated AssemblyInfo.cs and It's a signed commit

--- LuckyReclamationTrashCan/Properties/AssemblyInfo.cs
+++ LuckyReclamationTrashCan/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LuckyReclamationTrashCan")]
+[assembly: AssemblyTitle("Lucky Reclamation TrashCan")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LuckyReclamationTrashCan")]
+[assembly: AssemblyProduct("Stardew Valley Mods")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
