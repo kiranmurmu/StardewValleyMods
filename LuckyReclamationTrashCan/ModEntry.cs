685116500cd5c5b8d0449379824541c5abf69a60
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2021-06-17 21:55:32 +0530

    Added SaveLoaded event listener

--- LuckyReclamationTrashCan/ModEntry.cs
+++ LuckyReclamationTrashCan/ModEntry.cs
@@ -1,6 +1,8 @@
 ﻿using Harmony;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
+using System;
 
 namespace KiranMurmu.StardewValleyMods.LuckyReclamationTrashCan
 {
@@ -11,10 +13,17 @@ namespace KiranMurmu.StardewValleyMods.LuckyReclamationTrashCan
     /// <param name="helper">Provides simplified APIs for writing mods.</param>
     public override void Entry(IModHelper helper)
     {
+      helper.Events.GameLoop.SaveLoaded += new EventHandler<SaveLoadedEventArgs>(this.OnSaveLoaded);
+
       HarmonyInstance.Create(this.ModManifest.UniqueID).Patch(
         original: AccessTools.Method(typeof(Utility), nameof(Utility.getTrashReclamationPrice)),
         postfix: new HarmonyMethod(typeof(ModPatch), nameof(ModPatch.After_getTrashReclamationPrice))
       );
+    }
+
+    private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+    {
+      this.Monitor.Log($"Luck Level: {Game1.player.LuckLevel}, Added Luck: {Game1.player.addedLuckLevel}, Daily Luck: {Game1.player.DailyLuck}", LogLevel.Debug);
     }
   }
 }
